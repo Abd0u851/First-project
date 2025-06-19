368b46977be6cf70efb8a21fc1a5c56565c1cca0
Abd0u851 <abdullatifabaiss37@gmail.com>
2025-06-19 13:36:58 +0100

    My Website

---
+++ main.js
@@ -0,0 +1,19 @@
+/*
+document.getElementById('enter').addEventListener('submit', function(e) {
+    e.preventDefault();
+
+    const email = document.getElementById('email').Value;
+    const password = document.getElementById('password').Value;
+    if (email && password) {
+        localStorage.setItem('isLoggedIn', 'true');
+        window.location.href = "index.html#About Me";
+    }
+
+    else {
+        alert('the information is incorrect! Please try again.')
+    }
+});
+ يوجد مشكل في هاد الجافا سكريبت داكشي علاش انا دايرو مخفي
+(معلومة : هاد السكريبت راه ديال تسجيل الدخول)
+*/
+
